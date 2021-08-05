9515c5d3790ccada6b1984fdfa3c625b4a8601f0
ddooxthawsng <ddooxthawsng@gmail.com>
2021-08-05 19:57:40 +0700

    Update

---
+++ QLKFC/ChiTietHoaDonBanHang.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace QLKFC
+{
+    partial class ChiTietHoaDonBanHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTenNhanVien = new System.Windows.Forms.Label();
+            this.lblPos = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStoreID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.dgvChiTietHoaDonBanHang = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChiTietHoaDonBanHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTenNhanVien);
+            this.panel1.Controls.Add(this.lblPos);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblStoreID);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnDong);
+            this.panel1.Controls.Add(this.dgvChiTietHoaDonBanHang);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(517, 549);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblTenNhanVien
+            // 
+            this.lblTenNhanVien.AutoSize = true;
+            this.lblTenNhanVien.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTenNhanVien.ForeColor = System.Drawing.Color.Black;
+            this.lblTenNhanVien.Location = new System.Drawing.Point(254, 133);
+            this.lblTenNhanVien.Name = "lblTenNhanVien";
+            this.lblTenNhanVien.Size = new System.Drawing.Size(251, 38);
+            this.lblTenNhanVien.TabIndex = 42;
+            this.lblTenNhanVien.Text = "Tên Nhân Viên";
+            // 
+            // lblPos
+            // 
+            this.lblPos.AutoSize = true;
+            this.lblPos.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPos.ForeColor = System.Drawing.Color.Black;
+            this.lblPos.Location = new System.Drawing.Point(106, 133);
+            this.lblPos.Name = "lblPos";
+            this.lblPos.Size = new System.Drawing.Size(35, 38);
+            this.lblPos.TabIndex = 41;
+            this.lblPos.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(22, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 38);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Pos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(51, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(405, 29);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "----------------------------";
+            // 
+            // lblStoreID
+            // 
+            this.lblStoreID.AutoSize = true;
+            this.lblStoreID.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblStoreID.ForeColor = System.Drawing.Color.Black;
+            this.lblStoreID.Location = new System.Drawing.Point(309, 71);
+            this.lblStoreID.Name = "lblStoreID";
+            this.lblStoreID.Size = new System.Drawing.Size(35, 38);
+            this.lblStoreID.TabIndex = 38;
+            this.lblStoreID.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(106, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 38);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Store ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(503, 38);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Công ty LD TNHH KFC VietNam";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(51, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 38);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "----------------------";
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(411, 508);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(94, 29);
+            this.btnDong.TabIndex = 1;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // dgvChiTietHoaDonBanHang
+            // 
+            this.dgvChiTietHoaDonBanHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChiTietHoaDonBanHang.Location = new System.Drawing.Point(12, 186);
+            this.dgvChiTietHoaDonBanHang.Name = "dgvChiTietHoaDonBanHang";
+            this.dgvChiTietHoaDonBanHang.RowHeadersWidth = 51;
+            this.dgvChiTietHoaDonBanHang.RowTemplate.Height = 29;
+            this.dgvChiTietHoaDonBanHang.Size = new System.Drawing.Size(482, 305);
+            this.dgvChiTietHoaDonBanHang.TabIndex = 0;
+            // 
+            // ChiTietHoaDonBanHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 549);
+            this.Controls.Add(this.panel1);
+            this.Name = "ChiTietHoaDonBanHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chi Tiết Hóa Đơn";
+            this.Load += new System.EventHandler(this.ChiTietHoaDonBanHang_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChiTietHoaDonBanHang)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.DataGridView dgvChiTietHoaDonBanHang;
+        private System.Windows.Forms.Label lblTenNhanVien;
+        private System.Windows.Forms.Label lblPos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStoreID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QLKFC/ChiTietHoaDonBanHang.cs
@@ -0,0 +1,37 @@
+﻿using QLKFC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLKFC
+{
+    public partial class ChiTietHoaDonBanHang : Form
+    {
+        QLBHKFCContext db = new QLBHKFCContext();
+        
+        public ChiTietHoaDonBanHang()
+        {
+            InitializeComponent();
+        }
+
+        private void ChiTietHoaDonBanHang_Load(object sender, EventArgs e)
+        {
+            int check = (int)this.Tag;
+            var query = db.HoaDons.Where(x => x.MaHd == check).SingleOrDefault();
+            //lblTenNhanVien.Text = query.MaNvNavigation.TenNv;
+            lblPos.Text = query.Pos;
+            lblStoreID.Text = query.StoreId;
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
